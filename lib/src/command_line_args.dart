dcdc2c793690b9196875bc3e41c966aa2839bd11
Nathan Kolbas <nathan2000@outlook.com>
2022-12-02 19:12:47 -0600

    Updated command line args to use regex instead of space

--- lib/src/command_line_args.dart
+++ lib/src/command_line_args.dart
@@ -9,14 +9,15 @@ import 'package:ffi/ffi.dart';
 /// **IMPORTANT**: Remember to call [free] to release the memory that has
 /// been allocated for the pointers when done.
 class CommandLineArgs {
+  final RegExp argsSplit = RegExp(r'([-a-zA-Z0-9]+)|(".*?[^\\]")|("")');
   late final List<Pointer<Utf8>> _args;
   late final Pointer<Pointer<Utf8>> argv;
   late final int argc;
 
   CommandLineArgs(String argString) {
-    final argsList = argString.split(' ');
+    final argsList = argsSplit.allMatches(argString);
     argc = argsList.length;
-    _args = argsList.map((s) => s.toNativeUtf8().cast<Utf8>()).toList();
+    _args = argsList.map((s) => s.group(0)!.toNativeUtf8().cast<Utf8>()).toList();
     argv = malloc.allocate(sizeOf<Pointer<Utf8>>() * argc);
     for (int i = 0; i < argc; i++) {
       argv[i] = _args[i];
